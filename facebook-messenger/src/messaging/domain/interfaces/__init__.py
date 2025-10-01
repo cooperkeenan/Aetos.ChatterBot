9df85289164341ab62d1bdef87b4a0b9e108822c
Cooper Keenan <ckeenan@craneware.com>
2025-10-01 16:15:17 +0100

    bug fix

--- facebook-messenger/src/messaging/domain/interfaces/__init__.py
+++ facebook-messenger/src/messaging/domain/interfaces/__init__.py
@@ -5,7 +5,7 @@ Domain interfaces
 
 from .navigation_interface import INavigationService
 from .message_interface import IMessageService
-from .template_interface import ITemplateService
+
 
 __all__ = [
     'INavigationService',
