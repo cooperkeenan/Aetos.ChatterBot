2d20cfa8f76923af1e651ecf691f58a376aa282a
Cooper Keenan <ckeenan@craneware.com>
2025-10-06 15:32:45 +0100

    refactored code

--- facebook-messenger/src/core/__init__.py
+++ facebook-messenger/src/core/__init__.py
@@ -1,8 +1,5 @@
-# src/core/__init__.py
 """Core functionality package"""
 
 from .config_service import ConfigService, get_config
-from .rate_limiter import RateLimiter
 
-__all__ = ['ConfigService', 'get_config', 'RateLimiter']
-
+__all__ = ['ConfigService', 'get_config']
--- facebook-messenger/src/services/core/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Core functionality package"""
-
-from .config_service import ConfigService, get_config
-
-__all__ = ['ConfigService', 'get_config']
